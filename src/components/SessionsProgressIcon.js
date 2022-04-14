37acd65833566138ca58dbf6ac4197433bcd8e8d
Jeremy Mann <jmann277@gmail.com>
2022-04-14 10:35:43 -0700

    use progress bar instead of circle for number of sessions

--- src/components/SessionsProgressIcon.js
+++ src/components/SessionsProgressIcon.js
@@ -1,21 +1,24 @@
 import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
+import ProgressBar from "@ramonak/react-progress-bar";
+
 import "react-circular-progressbar/dist/styles.css";
 
 const SecondsProgressIcon = ({ nSessionsLeft, nSessions }) => {
   const color = "yellow";
-  const percentage = nSessionsLeft / nSessions * 100 ;
+  const percentage = (nSessionsLeft / nSessions) * 100;
   const text = nSessionsLeft === 0 ? "0" : nSessionsLeft;
 
   return (
-    <CircularProgressbar
-      value={percentage}
-      text={text}
-      styles={buildStyles({
-        textColor: "#fff",
-        pathColor: color,
-        tailColor: "rgba(255,255,255,.2)",
-      })}
-    />
+    <div style={{ paddingTop: "2rem" }}>
+      <ProgressBar
+        bgColor={color}
+        baseBgColor="white"
+        labelColor="black"
+        completed={percentage}
+        customLabel={text}
+                height="2rem"
+      />
+    </div>
   );
 };
 
